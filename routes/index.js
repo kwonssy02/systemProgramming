991f3b303cd4a86dcacdb4ec6dace96086ff7c58
KHC <kwonssy02@gmail.com>
2015-11-21 05:00:10 +0900

    더 불러오기 구현

--- routes/index.js
+++ routes/index.js
@@ -17,47 +17,72 @@ const addCommentURL = ("/addComment");
 const addCommentQUERY = ("INSERT INTO comment (imageId, author, content, regiDate) values(?, ?, ?, NOW())");
 const selectCommentsByImageIdQuery = ("SELECT imageId, author, content, DATE_FORMAT(regiDate, '%Y/%m/%d %H:%i:%s') as regiDate FROM comment WHERE imageId = ? ORDER BY regiDate ASC");
 
+const selectMorePostsURL = ("/morePosts/:imageId")
+const selectMorePostsQuery = ("SELECT imageId, location, DATE_FORMAT(regiDate, '%Y/%m/%d %H:%i:%s') as regiDate, TIMESTAMPDIFF(SECOND, regiDate, now()) secDiff FROM image where regiDate < (SELECT regiDate from image where imageId = ?) ORDER BY regiDate DESC LIMIT 10");
+const selectMoreCommentsQuery = ("SELECT A.imageId imageId, A.author author, A.content content, DATE_FORMAT(A.regiDate, '%Y/%m/%d %H:%i:%s') as regiDate FROM comment A LEFT JOIN image B ON A.imageId = B.imageId where B.regiDate < (SELECT regiDate from image where imageId = ?) ORDER BY A.regiDate ASC");
+
 router.get(homeURL, home);
 function home(req, res, next) {
-	var data = {};
-	data['title'] = '코메라: 냄새맡는 카메라';
-        connection.query(selectPostsQuery, function(err, rows, fields) {
-                if(err) {
-                        throw err;
-                }
-		data['posts'] = rows;
+    var data = {};
+    data['title'] = '코메라: 냄새맡는 카메라';
+    connection.query(selectPostsQuery, function(err, rows, fields) {
+        if(err) {
+            throw err;
+        }
+        data['posts'] = rows;
 		
 		connection.query(selectCommentsQuery, function(err, rows, fields) {
-                	if(err) {
-                        	throw err;
-                	}
-                	//res.send(rows);
-                	data['comments'] = rows;
-                	res.render('index', data);
-        	});
-        });
+        	if(err) {
+                throw err;
+        	}
+        	//res.send(rows);
+        	data['comments'] = rows;
+        	res.render('index', data);
+    	});
+    });
 
 }
 
 router.post(addCommentURL, addComment);
 function addComment(req, res, next) {
-        const imageId = req.body.imageId;
-        const author = req.body.author;
-	const content = req.body.content;
-	
-        const queryParams = [imageId, author, content];
-        connection.query(addCommentQUERY, queryParams, function(err, rows, fields) {
+    const imageId = req.body.imageId;
+    const author = req.body.author;
+    const content = req.body.content;
+    const queryParams = [imageId, author, content];
+    console.log(queryParams);
+    connection.query(addCommentQUERY, queryParams, function(err, rows, fields) {
+        if(err) {
+                throw err;
+        }
+		const queryParams2 = [imageId];
+		connection.query(selectCommentsByImageIdQuery, queryParams2, function(err, rows, fields) {
                 if(err) {
                         throw err;
                 }
-		const queryParams2 = [imageId];
-		connection.query(selectCommentsByImageIdQuery, queryParams2, function(err, rows, fields) {
-                        if(err) {
-                                throw err;
-                        }
-                        res.json(rows);
-                });
+                res.json(rows);
         });
+    });
+}
+
+router.get(selectMorePostsURL, selectMorePosts);
+function selectMorePosts(req, res, next) {
+    const imageId = req.params.imageId;    
+    const queryParams = [imageId];  
+    var result = {};
+    connection.query(selectMorePostsQuery, queryParams, function(err, rows, fields) {
+        if(err) {
+                throw err;
+        }
+        result["posts"] = rows;
+        const queryParams2 = [imageId];
+        connection.query(selectMoreCommentsQuery, queryParams2, function(err, rows2, fields) {
+                if(err) {
+                        throw err;
+                }
+                result["comments"] = rows2;
+                res.json(result);
+        });
+    });
 }
 
 module.exports = router;
